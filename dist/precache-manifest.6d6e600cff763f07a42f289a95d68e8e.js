37c6ee5fc328c57456987be6558bb3c4450d9747
AdrianBegic <adrianbegic1@gmail.com>
2022-04-04 14:46:50 -0400

    deploy

---
+++ dist/precache-manifest.6d6e600cff763f07a42f289a95d68e8e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2e45570c9f0d7024d3",
+    "url": "/abj-port-2/css/app.0e13a0ed.css"
+  },
+  {
+    "revision": "2b38b31a60bd6636efeb6e416c0a95ee",
+    "url": "/abj-port-2/fonts/MS Sans Serif 8pt.2b38b31a.ttf"
+  },
+  {
+    "revision": "bd256b1fcc78cb6be0057998fe3b2cf6",
+    "url": "/abj-port-2/img/Windows_98_logo.svg.bd256b1f.webp"
+  },
+  {
+    "revision": "efaa04e49d24b19dc97df486ecdd0bb1",
+    "url": "/abj-port-2/img/git.efaa04e4.webp"
+  },
+  {
+    "revision": "a129cfba13d83e339cba4f8cb2bd0e99",
+    "url": "/abj-port-2/img/npm.a129cfba.webp"
+  },
+  {
+    "revision": "6241bf084cf3262d1d85a6ae5511d7cd",
+    "url": "/abj-port-2/index.html"
+  },
+  {
+    "revision": "5f2e45570c9f0d7024d3",
+    "url": "/abj-port-2/js/app.a2e9142d.js"
+  },
+  {
+    "revision": "80aba62b1ae8d4f4e25c",
+    "url": "/abj-port-2/js/chunk-vendors.64bc0c78.js"
+  },
+  {
+    "revision": "4815c9fd7015a61964ff05a7710f812b",
+    "url": "/abj-port-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/abj-port-2/robots.txt"
+  }
+]);
