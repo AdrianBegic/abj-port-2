cc730f96cd1e4193b6584f822f3592f85153ec3b
AdrianBegic <adrianbegic1@gmail.com>
2022-04-04 14:46:08 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,6 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
 
@@ -20,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+ git push -f git@github.com:AdrianBegic/abj-port-2.git main:gh-pages
 
 cd -
